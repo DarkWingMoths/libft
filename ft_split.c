20548e759bcf95dd07caf553b7077eb4fff7b236
DarkwingMoths <moths87@gmail.com>
2022-05-02 08:56:45 -0400

    modmai

--- ft_split.c
+++ ft_split.c
@@ -6,51 +6,79 @@
 /*   By: mgagnon <mgagnon@student.42quebec.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 14:43:51 by mgagnon           #+#    #+#             */
-/*   Updated: 2022/04/30 16:33:29 by mgagnon          ###   ########.fr       */
+/*   Updated: 2022/05/01 23:24:26 by mgagnon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-static size_t	ft_ind_fndr(char const *s, char c)
+static size_t	ft_word_count(char const *s, char c)
 {
 	size_t index;
+	size_t	word_count;
 
 	index = 0;
-	while (s[index] != c)
+	word_count = 0;
+	while (s[index])
+	{
+		if (s[index] == c)
+			word_count++;
 		index++;
+	}
+	word_count++;
+	printf("word count is = %i\n", word_count);
+	return (word_count);
+}
+
+static size_t	ft_word_size(char const *s, char c)
+{
+	size_t	index;
+	
+	index = 0;
+	while(s[index] != c)
+		index++;
+	printf("word size is = %i\n", (index));
 	return (index);
 }
+	
 char	**ft_split(char const *s, char c)
 {
-	char	**split_rslt;
-	size_t	split_point;
+	char	**split_result;
+	size_t	next_array;
 	size_t	index;
 	size_t	s_len;
+	size_t	count;
+	size_t	size;
 
+	index = 0;
 	if (!s || !c)
 		return (NULL);
 	s_len = ft_strlen(s);
-	index = 0;
-	split_point = ft_ind_fndr(s, c);
-	while (split_point < s_len && !ft_strchr(s, c))
-		split_point++;
-	split_rslt[0][] = malloc(sizeof (char) * split_point);
-	split_rslt[1][] = malloc(sizeof (char) * (s_len - split_point));
-	if (!split_rslt[0] || !split_rslt[1])
+	count = ft_word_count(s, c);
+	split_result = malloc (sizeof (char) * count);
+	if (!split_result)
 		return (NULL);
-	while (index < split_point)
+	next_array = 0;
+	while (index <= count && next_array <= s_len)
 	{
-		split_rslt[0][index] = s[index];
+		size = (ft_word_size(&s[next_array], c) + 1);
+		split_result[index] = malloc(sizeof (char) * size);
+		if (!split_result[index])
+			return (NULL);
+		ft_substr(s, next_array, size);
+		next_array = size + 1;
 		index++;
 	}
-	split_rslt[0][index] = '\0';
-	split_point++;
-	while (split_point < s_len)
-	{
-		split_rslt[1][split_point] = s[split_point];
-		split_point++;
-	}
-	split_rslt[1][split_point] = '\0';
-	return (split_rslt);
+	return (split_result);
+}
+
+int	main(void)
+{
+	char const	*s = "abcd,efgh,ij,klmno,pqrstuv,w,xyz";
+	char		c;
+
+	c = ',';
+	ft_split(s, c);
+	return (0);
 }
